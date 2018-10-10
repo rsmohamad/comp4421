ec205e28ec0eb7bb19da41db1d4f90049e77137b
Randitya <rsmohamad@ust.hk>
2018-10-10 22:42:34 +0800

    dft

--- ass1/code/dftTest.m
+++ ass1/code/dftTest.m
@@ -2,8 +2,19 @@ function dftTest()
   pkg load image
   img = imread('../73.png');
   dft_img = dft_2d(img, 'DFT');
+  fft_img = fft2(img);
   dft_spectrum = log10(dft_img + 1);
   dft_spectrum = mat2gray(dft_spectrum);
+  
+  difference = sum(abs(dft_img - fft_img)(:))
+  
+  idft_img = dft_2d(fft_img, 'IDFT');
+
+  % Transform idft_img to a real-value matrix
+  real_img = real(idft_img);
+  real_img = mat2gray(real_img);
+  
   figure,
   subplot(121), imshow(dft_spectrum), title('Fourier Spectrum')
+  subplot(122), imshow(real_img), title('Image after IDFT')
   
--- ass1/code/dft_2d.m
+++ ass1/code/dft_2d.m
@@ -1,26 +1,56 @@
 function img_result = dft_2d(img_input, flag)
+  pkg load signal  
   img_input = double(img_input);
-  img_result = zeros(size(img_input));
-  [N, M] = size(img_input)
+  [N, M] = size(img_input);
+  dft_R=zeros(N,M); 
+  img_result=zeros(N,M); 
   
-  if flag == 'DFT'
+  if strcmp(flag, 'DFT') == 1    
     
-    for freq_index = 1:1:numel(img_result)
-      [v, u] = ind2sub(size(img_result), freq_index);
-      
-      res = 0;
-      for spatial_index = 1:1:numel(img_input)
-        [y, x] = ind2sub(size(img_input), spatial_index);
-        angle = 2 * pi * ((u*x)/M + (v*y)/N);   
-        res = res + (img_input(spatial_index) * cos(angle));
+    % fftshift  
+    shift_mat = ones(size(img_input));  
+    for i = 1:1:numel(shift_mat)
+      [y, x] = ind2sub(size(shift_mat), i);
+      if mod(x+y, 2) == 1
+        shift_mat(i) = -1;
       end
-      
-      res = res / numel(img_input)
-      img_result(freq_index) = res;
-      
+    end
+    img_input = img_input .* shift_mat;
+    
+    dft_mat = dftmtx(M);  
+    for row = 1:1:N
+      vec = img_input(row, :)';
+      dft_R(row, :) = (dft_mat' * vec)'  ;
+    end
+    
+    dft_mat = dftmtx(N);    
+    for col = 1:1:M
+      vec = dft_R(:, col);
+      img_result(:, col) = dft_mat * vec;
+    end
+    
+  else
+    
+    % ifftshift
+    img_input = [img_input(:, ((M/2)+1):M) img_input(:, 1:M/2)];
+    img_input = [img_input(((N/2)+1):N, :); img_input(1:N/2, :)];
+    
+    dft_mat = dftmtx(M);
+    for row = 1:1:N
+      vec = img_input(row, :)';
+      dft_R(row, :) = (dft_mat * vec)'  ;
+    end
+    
+    dft_mat = dftmtx(N);    
+    for col = 1:1:M
+      vec = dft_R(:, col);
+      img_result(:, col) = dft_mat' * vec;
     end
     
   end
+ 
+
+ 
        
     
  
